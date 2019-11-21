376ff8cb95f9fad4ac4f911fbfafc4bfb6a49d17
YOSHIOKA Eiichiro <yoshioka-e@kamakura-net.co.jp>
2019-11-21 12:24:53 +0900

    ベースの作成

---
+++ lib/ggenki_web/controllers/bot_controller.ex
@@ -0,0 +1,36 @@
+defmodule GgenkiWeb.BotController do
+  use GgenkiWeb, :controller
+
+  def line_callback(conn, %{"events" => events}) do
+    %{"message" => message } = List.first(events)
+    %{"source" => source } = List.first(events)
+    events = List.first(events)
+    endpoint_uri = "https://api.line.me/v2/bot/message/reply"
+
+    json_data = %{
+                  replyToken: events["replyToken"],
+                  messages: [
+                    %{
+                      type: "text",
+                      text: message["text"] # 受信したメッセージをそのまま返す
+                    }
+                  ]
+                } |> Poison.encode!
+
+    headers = %{
+      "Content-Type" => "application/json",
+      "Authorization" => "Bearer ${" <> System.get_env("LINE_ACCESS_TOKEN") <> "}"   #メッセージ送受信設定|>アクセストークンからアクセストークンを取得
+    }
+
+    case HTTPoison.post(endpoint_uri, json_data, headers) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        IO.puts body
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+
+    send_resp(conn, :no_content, "")
+  end
+end
