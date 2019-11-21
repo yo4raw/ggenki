dbb73986f7684b6c8eaa12816ec97f11d796fc1a
YOSHIOKA Eiichiro <yoshioka-e@kamakura-net.co.jp>
2019-11-21 18:00:36 +0900

    fix

--- lib/ggenki_web/controllers/bot_controller.ex
+++ lib/ggenki_web/controllers/bot_controller.ex
@@ -12,7 +12,7 @@ defmodule GgenkiWeb.BotController do
     endpoint_uri = "https://api.line.me/v2/bot/message/reply"
 
     # LINEメッセージの保存
-    message = Message.insert(%Message{user: source["userId"],body: List.to_string(events)})
+    message = Message.insert(%Message{user: source["userId"],body: Poison.encode(events)})
 
 
     json_data = %{
