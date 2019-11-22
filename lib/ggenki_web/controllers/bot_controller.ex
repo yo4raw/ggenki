5e9d213da665760a08b40f109523115002471e9c
YOSHIOKA Eiichiro <yoshioka-e@kamakura-net.co.jp>
2019-11-22 19:42:06 +0900

    fix

--- lib/ggenki_web/controllers/bot_controller.ex
+++ lib/ggenki_web/controllers/bot_controller.ex
@@ -38,8 +38,8 @@ defmodule GgenkiWeb.BotController do
 
     IO.inspect DateTime.from_naive(message.inserted_at,"Etc/UTC")
     IO.inspect DateTime.now("Etc/UTC")
-    target_message_datetime = DateTime.from_naive(message.inserted_at,"Etc/UTC")
-    IO.inspect Timex.shift(target_message_datetime, hours: interval_hour)
+{_,target_message_datetime} = DateTime.from_naive(message.inserted_at,"Etc/UTC")
+    IO.inspect Timex.shift(target_message_datetime, hours: interval_hour,  minutes: 0)
     IO.inspect Timex.now
 
     IO.inspect DateTime.compare(Timex.shift(message.inserted_at |> DateTime.from_naive("Etc/UTC"), hours: interval_hour),Timex.now)
