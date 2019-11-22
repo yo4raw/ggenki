7580fc7d5ce8526a23253ec0698ed7a06225e720
YOSHIOKA Eiichiro <yoshioka-e@kamakura-net.co.jp>
2019-11-22 15:19:03 +0900

    fix

--- lib/ggenki_web/controllers/bot_controller.ex
+++ lib/ggenki_web/controllers/bot_controller.ex
@@ -17,31 +17,6 @@ defmodule GgenkiWeb.BotController do
     # LINEメッセージの保存
     Ggenki.Repo.insert(%Message{user: source["userId"],body: Poison.encode!(events)})
 
-
-    json_data = %{
-                  replyToken: events["replyToken"],
-                  messages: [
-                    %{
-                      type: "text",
-                      text: message["text"] # 受信したメッセージをそのまま返す
-                    }
-                  ]
-                } |> Poison.encode!
-
-    headers = %{
-      "Content-Type" => "application/json",
-      "Authorization" => "Bearer " <> System.get_env("LINE_ACCESS_TOKEN") #メッセージ送受信設定|>アクセストークンからアクセストークンを取得
-    }
-
-    case HTTPoison.post(endpoint_uri, json_data, headers) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        IO.puts body
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts "Not found :("
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
-    end
-
     send_resp(conn, :no_content, "")
   end
 
