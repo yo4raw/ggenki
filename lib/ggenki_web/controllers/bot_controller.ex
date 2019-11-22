8f55dbed6046cd6f4ad03f19029fbb39331918a6
YOSHIOKA Eiichiro <yoshioka-e@kamakura-net.co.jp>
2019-11-22 13:29:18 +0900

    fix

--- lib/ggenki_web/controllers/bot_controller.ex
+++ lib/ggenki_web/controllers/bot_controller.ex
@@ -64,6 +64,7 @@ defmodule GgenkiWeb.BotController do
 
     #最後の発言から特定の時間が経過していたら
     if Timex.shift(message.inserted_at, hour: interval_hour) < Timex.now do
+      IO.puts "時間経過"
       endpoint_uri = "https://api.line.me/v2/bot/message/reply"
       json_data = %{
                     groupId: line_group_id,
