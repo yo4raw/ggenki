37116c7ef2d41357f22ddffe3b9b4e33f9230b54
YOSHIOKA Eiichiro <yoshioka-e@kamakura-net.co.jp>
2019-11-22 10:57:38 +0900

    fix

--- lib/ggenki_web/controllers/bot_controller.ex
+++ lib/ggenki_web/controllers/bot_controller.ex
@@ -50,7 +50,7 @@ defmodule GgenkiWeb.BotController do
     #最後の発言を確認する時間間隔
     interval_hour = 1
 
-    message = Message
+    message = Messages
       |> last(:inserted_at)
       |> where(user: System.get_env("TARGET_LINE_USER"))
       |> Repo.one
